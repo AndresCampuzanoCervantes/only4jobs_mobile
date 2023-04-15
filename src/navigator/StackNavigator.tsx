cafc0b6d7e81b064b1eb8181a9625d0792aeae47
GNoNameG <gabrielarias14@hotmail.com>
2023-04-15 01:25:22 -0500

    User profile

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Login} from '../screens/login/';
-import Register from '../screens/login/Register';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Login} from '../screens/login';
+import Register from '../screens/login/register';
+import Profile from '../screens/user_profile/Profile';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Screen name="login" component={Login} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="login" component={Profile} />
       <Stack.Screen name="registro" component={Register} />
       <Stack.Screen name="recuperar-contraseña" component={Login} />
       <Stack.Screen name="home" component={Login} />
     </Stack.Navigator>
   );
 };
-
 
 //make this component available to the app
 export default StackNavigator;
